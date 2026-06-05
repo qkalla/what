8279764978fd9edc4e928f65df883991539c375b
loplop <aliilaamin12331@gmail.com>
2026-06-05 14:45:41 +0400

    Refactor index.js for improved functionality

--- index.js
+++ index.js
@@ -68,7 +68,7 @@ client.on('ready', () => {
 
 // لوحة التحكم بالبوت من الواتساب
 client.on('message_create', async (msg) => {
-    const masterAdmin = '37433241430@c.us'; 
+    const masterAdmin = '37493290481@c.us'; 
     if (msg.from !== masterAdmin) return;
 
     if (msg.body.startsWith('!addjob ')) {
